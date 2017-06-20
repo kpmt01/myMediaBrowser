97c6bc7f387e64e92f3cb99720cca09eb5964df5
kpmt01 <kpmt01@gmail.com>
2017-06-20 17:21:36 +0300

    Update mediaSearch.php

--- mediaSearch.php
+++ mediaSearch.php
@@ -1,19 +1,21 @@
 <?php
 
 	function fileSearch($dir = ".\\",$json = ''){
-    	//echo "<span style='color:green;'>".$json."</span>";
 		$dir = $dir."\\";
+		$json2 = "";
+
 		if (is_dir($dir)) {
 		    if ($dit = opendir($dir)) {
 		        while (($dosya = readdir($dit)) !== false) {
 		            if(filetype($dir. $dosya) === "dir" and $dosya !== "." and $dosya !== ".."){
 		            	echo $dir. $dosya."<br>";
-		            	$json2 = fileSearch($dir. $dosya,$json);
+		            	$json2 .= fileSearch($dir. $dosya,$json);
 		            }else if($dosya !== "." and $dosya !== ".."){
 		            	echo $dir.$dosya." <span style='color:red;'>".mime_content_type($dir. $dosya)."</span> <br>";
-		            	$json ='{"'.$dir.$dosya.'","'.mime_content_type($dir. $dosya).'"}<br>';
+		            	$json .='{"'.$dir.$dosya.'","'.mime_content_type($dir. $dosya).'"}<br>';
 		            }
 		        }
+		            $json = $json . $json2;
 		        closedir($dit);
 		    }
 		}
